c11139b4ff25583890e79e31e5dc86689c32b8cd
Agnes Pilar <agnes.pilar@yahoo.de>
2023-01-17 15:12:00 +0100

    Style with emotion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import randomColor from 'randomcolor';
 import { useState } from 'react';
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+
+const divStyles = () => css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const buttonStyles = () => css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function App() {
   const [color, setColor] = useState(randomColor());
   return (
     <div>
-      <h1>Random Color Generator</h1>
+      <h1 css={divStyles}>Random Color Generator</h1>
       <button
+        css={buttonStyles}
         onClick={() => {
           setColor(randomColor());
         }}
       >
         Generate
       </button>
-      <div
-        style={{
-          backgroundColor: color,
-        }}
-      >
+      <div css={divStyles()} style={{ backgroundColor: color }}>
         Generated Color: {color}
       </div>
     </div>
